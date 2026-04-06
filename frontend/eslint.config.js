336d977a5d281dd0e12ae868f5fd42b7a2bb708f
Karl Saint Lucy <12864843+karlsaintlucy@users.noreply.github.com>
2026-04-06 10:03:03 -0400

    chore: add eslint-plugin-jsx-a11y for accessibility audits

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,11 +3,13 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { ignores: ['dist', 'node_modules', '.vite'] },
   js.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
